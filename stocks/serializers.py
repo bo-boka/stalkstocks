c8aa0520c278ca7a425f0e86477c63d6cbc76446
Sarah Boka <boka.sarah@gmail.com>
2017-06-24 00:40:00 -0400

    views and serializers

--- stocks/serializers.py
+++ stocks/serializers.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from rest_framework import serializers
 from .models import Stock
+
 
 class StockSerializer(serializers.ModelSerializer):
 
